d3eb9619650dc614513882373ed7cd77ceadb066
07balajee <harisbalajee61@gmail.com>
2025-10-29 10:24:42 +0530

    Definition of the Project Structure

---
+++ src/test/java/com/example/ComplaintDesk/ComplaintDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ComplaintDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
